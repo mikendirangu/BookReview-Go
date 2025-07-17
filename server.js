2550918dfaa265291c05fe31a439aeb52e641777
Big mike <111752697+mikendirangu@users.noreply.github.com>
2025-07-17 22:01:03 +0300

    Fix start script to use Script.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
+const jsonserver = require("json-server");
+const server = jsonserver.create();
+const router = jsonserver.router("db.json");
+const middlewares = jsonserver.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
 server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
+  console.log(`JSON server is running on port ${port}`);
 });
