c5718920ea5386451ad77ecf703b85bb51fd22a9
Big mike <111752697+mikendirangu@users.noreply.github.com>
2025-07-17 19:34:58 +0300

    Fix It Server.js

--- Server.js
+++ Server.js
@@ -1,12 +1,22 @@
-const jsonServer = require('json-Server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-Server.use(middlewares);
-Server.use(router);
+app.get('/', (req, res) => res.send('Hello from server.js!'));
 
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+// const jsonServer = require('json-Server');
+// const server = jsonServer.create();
+// const router = jsonServer.router('db.json');
+// const middlewares = jsonServer.defaults();
+
+// Server.use(middlewares);
+// Server.use(router);
+
+// const port = process.env.PORT || 3000;
+// server.listen(port, () => {
+//   console.log(`JSON Server is running on port ${port}`);
+// });
